3a7997c78af3e0a6823dabf734f1ceefc40c5c66
Tim Wright <timwright12@gmail.com>
2013-11-09 12:28:57 -0500

    initial commit

---
+++ assets/js/polybutton.js
@@ -0,0 +1,85 @@
+/*
+  Polybutton - An accessibile version of links that look like buttons
+  License: MIT/GPLv2
+  Author: Tim Wright, csskarma.com
+  Script structure inspired by Scott Jehl's Picturefill
+*/
+
+;(function( b ){
+
+  "use strict";
+  
+  b.polybutton = function() {
+
+    var btn = b.document.querySelectorAll( "span[data-accessible]" );
+    var count = btn.length;
+    var i, obj, access, href, type, value, initClass, genBtn;
+
+    // Loop through the polybuttons
+    for ( i = 0; i < count; i = i + 1 ) {
+      
+      // store some variables
+      obj = btn[i];
+      href = obj.getAttribute('data-href');
+      access = obj.getAttribute('data-accessible');
+      type = obj.getAttribute('data-type');
+      value = obj.getAttribute('data-value');
+      initClass = obj.getAttribute('data-class');
+      
+      // make sure we're dealing with a button
+      if( access === 'button' ) {
+      
+        genBtn = document.createElement( 'button' );
+        
+        // build out the new button asset
+        genBtn.setAttribute("class", initClass);
+        genBtn.setAttribute("type", type);
+        genBtn.setAttribute("data-url", href);
+        genBtn.innerHTML = value;
+        
+        // add the new button into the DOM
+        obj.appendChild(genBtn);
+        
+        // check to see if we can use addEventListener on the new button, then tie a click event to it
+        if( genBtn.addEventListener ) {
+        
+          genBtn.addEventListener('click', function(e) {
+  
+            window.location = this.getAttribute('data-url');
+            
+          }, false);
+        
+        } else if( genBtn.attachEvent ) {
+        
+          genBtn.attachEvent( "onload", function(e){
+            window.location = this.getAttribute('data-url');
+          });
+        
+        } // end if addEventListener
+      
+      } // end if button
+      
+    } // end for loop
+    
+  }; // end polybutton
+  
+  // execute on DOM ready (b.load as a fallback)
+  if( b.addEventListener ){
+
+    b.addEventListener( "DOMContentLoaded", function(){
+      
+      // execute polybutton
+      b.polybutton();
+      
+      // only do it once
+      b.removeEventListener( "load", b.polybutton, false );
+      
+    }, false );
+  
+  } else if( b.attachEvent ){
+
+    b.attachEvent( "onload", b.polybutton );
+
+  } // end initial load event
+  
+}( this ));
